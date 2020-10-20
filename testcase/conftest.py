74d027fd408fc175fd7eac979a702d10f705e337
zhangcaiyan-0609 <58460993+zhangcaiyan-0609@users.noreply.github.com>
2020-10-20 11:14:46 +0800

    Add files via upload

---
+++ testcase/conftest.py
@@ -0,0 +1,57 @@
+"""
+File name: conftest.py.py
+Author: caiyan
+Email: caiyanyifei@163.com
+Time : 2020/10/12 11:02 上午
+"""
+import pytest
+from selenium.webdriver import Chrome
+from common.handle_config import conf
+from page.Index_page import IndexPage
+from page.login_page import LoginPage
+from page.invest_page import InvestPage
+from page.user_page import UserPage
+from selenium import webdriver
+import os
+#读取配置文件中的配置，判断是否是以无头浏览器模式运行
+#chromedriver = "/usr/local/bin/chromedriver"
+#os.environ["webdriver.chrome.driver"] = chromedriver
+
+def create_dirver():
+    boll = conf.getboolean("run","headless")
+    if boll:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(options=options,executable_path="/usr/local/bin/chromedriver")
+    else:
+        driver = Chrome(executable_path="/usr/local/bin/chromedriver")
+    return driver
+
+@pytest.fixture(scope="class")
+def login_setup():
+    driver = create_dirver()
+    driver.implicitly_wait(15)
+    driver.get(conf.get('env', 'base_url') + conf.get('env', 'url_path'))
+    login_page = LoginPage(driver)
+    index_page = IndexPage(driver)
+    yield login_page,index_page
+    driver.quit()
+@pytest.fixture(scope="class")
+def invest_setup():
+    driver = create_dirver()
+    driver.maximize_window()
+    driver.implicitly_wait(15)
+    driver.get(conf.get('env', 'base_url') + conf.get('env', 'url_path'))
+    login_page = LoginPage(driver)
+    # 登录
+    login_page.login(conf.get("test_data", "phone"), conf.get("test_data", "pwd"))
+    # 创建首页对象
+    index_page = IndexPage(driver)
+    # 点击抢投标
+    index_page.click_invest_loan()
+    # 进入投资页面
+    invest_page = InvestPage(driver)
+    user_page = UserPage(driver)
+    yield invest_page,user_page
+    driver.quit()
